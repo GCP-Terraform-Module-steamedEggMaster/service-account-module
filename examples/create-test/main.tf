1128b835bb0f387042d648818694c24dcb36b5c7
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-15 08:37:56 +0900

    fix : remove unsupported argument

--- examples/create-test/main.tf
+++ examples/create-test/main.tf
@@ -8,5 +8,4 @@ module "service_account" {
   display_name                 = "Test Service Account"                                  # 서비스 계정의 표시 이름
   description                  = "This is a service account for demonstration purposes." # 서비스 계정 설명
   disabled                     = false                                                   # 서비스 계정 비활성화 여부
-  create_ignore_already_exists = true                                                    # 이미 존재하는 경우 무시
 }
